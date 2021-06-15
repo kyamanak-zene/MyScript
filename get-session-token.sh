3c7c10de4a33b9dc8980bb9d00da0416680fc994
kyamanak83 <kyamanak@zene.co.jp>
2021-06-15 23:16:47 +0900

    update

--- get-session-token.sh
+++ get-session-token.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 既に設定されている場合、設定し直す
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_SESSION_TOKEN
+
 iam=$1
 token_code=$2
 user_name=`whoami`
@@ -10,10 +15,11 @@ echo "[INFO] TOKEN_CODE: $token_code"
 
 resp=`aws sts get-session-token --serial-number arn:aws:iam::$iam:mfa/$user_name --token-code $token_code`
 
-ACCESS_KEY_ID=`echo $resp | jq .Credentials.AccessKeyId | tr -d '"'`
-SECRET_ACCESS_KEY=`echo $resp | jq .Credentials.SecretAccessKey | tr -d '"'`
-SESSION_TOKEN=`echo $resp | jq .Credentials.SessionToken | tr -d '"'`
+access_key_id=`echo $resp | jq .Credentials.AccessKeyId | tr -d '"'`
+secret_access_key=`echo $resp | jq .Credentials.SecretAccessKey | tr -d '"'`
+session_token=`echo $resp | jq .Credentials.SessionToken | tr -d '"'`
 
-export AWS_ACCESS_KEY_ID="$ACCESS_KEY_ID"
-export AWS_SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY"
-export AWS_SESSION_TOKEN="$SESSION_TOKEN"
+# 環境変数名は AWS から指定がある
+export AWS_ACCESS_KEY_ID="$access_key_id"
+export AWS_SECRET_ACCESS_KEY="$secret_access_key"
+export AWS_SESSION_TOKEN="$session_token"
